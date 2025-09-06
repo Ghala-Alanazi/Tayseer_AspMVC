e9716c5f2043b6518bcbe12f43c0be6f2b3ca738
Ghala-Alanazi <gala05995@gmail.com>
2025-09-06 21:42:33 +0300

    Add project files.

---
+++ Tayseer_AspMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tayseer_AspMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
