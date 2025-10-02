75fc30590d301bde4f738108cdc82922f29a9590
Ghala-Alanazi <gala05995@gmail.com>
2025-10-02 10:35:43 +0300

    Controller,View

---
+++ Tayseer_AspMVC/Controllers/DisabilityCenterController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Tayseer_AspMVC.Models;
+using Tayseer_AspMVC.Repository.Base;
+
+namespace Tayseer_AspMVC.Controllers
+{
+    public class DisabilityCenterController : Controller
+    {
+
+        
+            private readonly IUnitOfWork _unitOfWork;
+
+            public DisabilityCenterController(IUnitOfWork unitOfWork)
+            {
+                _unitOfWork = unitOfWork;
+
+            }
+
+            //-----------------------------------------------------------------------------------------------------------------------
+            // Disability Center
+
+            public IActionResult DisabilityCenter()
+            {
+                var disabilityCenters = _unitOfWork.RepoCenters.DisabilityCenter();
+                return View(disabilityCenters);
+            }
+
+            public IActionResult CreateDisabilityCenter()
+            {
+                // نجيب المراكز والإعاقات من قاعدة البيانات
+                ViewBag.Centers = new SelectList(_unitOfWork.Centers.FindAll(), "Id", "Name");
+                ViewBag.Disabilities = new SelectList(_unitOfWork.Disabilitys.FindAll(), "Id", "Name");
+
+                return View();
+            }
+
+            [HttpPost]
+            public IActionResult CreateDisabilityCenter(DisabilityCenter disabilityCenter)
+            {
+                if (ModelState.IsValid)
+                {
+                    _unitOfWork.DisabilityCenters.Add(disabilityCenter);
+                    _unitOfWork.Save();
+
+                    TempData["Add"] = "تم اضافة البيانات بنجاح";
+                    return RedirectToAction(nameof(DisabilityCenter));
+                }
+
+                // لو فيه خطأ نرجع القوائم
+                ViewBag.Centers = new SelectList(_unitOfWork.Centers.FindAll(), "Id", "Name", disabilityCenter.CentersId);
+                ViewBag.Disabilities = new SelectList(_unitOfWork.Disabilitys.FindAll(), "Id", "Name", disabilityCenter.DisabilityId);
+
+                return View(disabilityCenter);
+            }
+
+
+            public IActionResult EditDisabilityCenter(int id)
+            {
+                var disabilityCenter = _unitOfWork.DisabilityCenters.FindById(id);
+                ViewBag.Centers = new SelectList(_unitOfWork.Centers.FindAll(), "Id", "Name", disabilityCenter.CentersId);
+                ViewBag.Disabilities = new SelectList(_unitOfWork.Disabilitys.FindAll(), "Id", "Name", disabilityCenter.DisabilityId);
+                return View(disabilityCenter);
+            }
+
+            [HttpPost]
+            public IActionResult EditDisabilityCenter(DisabilityCenter disabilityCenter)
+            {
+                if (ModelState.IsValid)
+                {
+                    _unitOfWork.DisabilityCenters.Update(disabilityCenter);
+                    _unitOfWork.Save();
+
+                    TempData["Edit"] = "تم تعديل البيانات بنجاح";
+                    return RedirectToAction(nameof(DisabilityCenter));
+                }
+
+                // لو فيه خطأ نرجع القوائم
+                ViewBag.Centers = new SelectList(_unitOfWork.Centers.FindAll(), "Id", "Name", disabilityCenter.CentersId);
+                ViewBag.Disabilities = new SelectList(_unitOfWork.Disabilitys.FindAll(), "Id", "Name", disabilityCenter.DisabilityId);
+
+                return View(disabilityCenter);
+            }
+
+
+            public IActionResult DeleteDisabilityCenter(int id)
+            {
+                var disabilityCenter = _unitOfWork.DisabilityCenters.FindById(id);
+                ViewBag.Centers = new SelectList(_unitOfWork.Centers.FindAll(), "Id", "Name", disabilityCenter.CentersId);
+                ViewBag.Disabilities = new SelectList(_unitOfWork.Disabilitys.FindAll(), "Id", "Name", disabilityCenter.DisabilityId);
+                return View(disabilityCenter);
+            }
+
+            [HttpPost]
+            public IActionResult DeletePostDisabilityCenter(int id)
+            {
+                var disabilityCenter = _unitOfWork.DisabilityCenters.FindById(id);
+                _unitOfWork.DisabilityCenters.Delete(disabilityCenter);
+                _unitOfWork.Save();
+
+                TempData["Delete"] = "تم حذف البيانات بنجاح";
+                return RedirectToAction(nameof(DisabilityCenter));
+            }
+        }
+
+    }
---
+++ Tayseer_AspMVC/Controllers/DisabilitySchoolController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Tayseer_AspMVC.Models;
+using Tayseer_AspMVC.Repository;
+using Tayseer_AspMVC.Repository.Base;
+
+namespace Tayseer_AspMVC.Controllers
+{
+    public class DisabilitySchoolController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IRepoSchool _repoSchool;
+
+
+        public DisabilitySchoolController(IUnitOfWork unitOfWork,  IRepoSchool repoSchool)
+        {
+            _unitOfWork = unitOfWork;
+            _repoSchool = repoSchool;
+        }
+
+
+
+
+        //-----------------------------------------------------------------------------------------------------------------------
+        // Disability School
+
+        public IActionResult DisabilitySchool()
+        {
+            var Disability = _unitOfWork.Schools.DisabilitySchool();
+
+            return View(Disability);
+        }
+
+        public IActionResult CreateDisabilitySchool()
+        {
+            // نجيب المدارس والإعاقات من قاعدة البيانات
+            ViewBag.Schools = new SelectList(_unitOfWork.Schools.FindAll(), "Id", "Name");
+            ViewBag.Disabilities = new SelectList(_unitOfWork.Disabilitys.FindAll(), "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateDisabilitySchool(DisabilitySchool disabilitySchool)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.DisabilitySchools.Add(disabilitySchool);
+                _unitOfWork.Save();
+
+                TempData["Add"] = "تم اضافة البيانات بنجاح";
+                return RedirectToAction(nameof(DisabilitySchool));
+            }
+
+            // لو فيه خطأ نرجع القوائم
+            ViewBag.Schools = new SelectList(_unitOfWork.Schools.FindAll(), "Id", "Name", disabilitySchool.SchoolId);
+            ViewBag.Disabilities = new SelectList(_unitOfWork.Disabilitys.FindAll(), "Id", "Name", disabilitySchool.DisabilityId);
+
+            return View(disabilitySchool);
+        }
+
+        public IActionResult EditDisabilitySchool(int id)
+        {
+            var disabilitySchool = _unitOfWork.DisabilitySchools.FindById(id);
+            ViewBag.Schools = new SelectList(_unitOfWork.Schools.FindAll(), "Id", "Name", disabilitySchool.SchoolId);
+            ViewBag.Disabilities = new SelectList(_unitOfWork.Disabilitys.FindAll(), "Id", "Name", disabilitySchool.DisabilityId);
+            return View(disabilitySchool);
+        }
+
+        [HttpPost]
+        public IActionResult EditDisabilitySchool(DisabilitySchool disabilitySchool)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.DisabilitySchools.Update(disabilitySchool);
+                _unitOfWork.Save();
+
+                TempData["Edit"] = "تم تعديل البيانات بنجاح";
+                return RedirectToAction(nameof(DisabilitySchool));
+            }
+
+            // لو فيه خطأ نرجع القوائم
+            ViewBag.Schools = new SelectList(_unitOfWork.Schools.FindAll(), "Id", "Name", disabilitySchool.SchoolId);
+            ViewBag.Disabilities = new SelectList(_unitOfWork.Disabilitys.FindAll(), "Id", "Name", disabilitySchool.DisabilityId);
+
+            return View(disabilitySchool);
+        }
+
+        public IActionResult DeleteDisabilitySchool(int id)
+        {
+            var disabilitySchool = _unitOfWork.DisabilitySchools.FindById(id);
+            ViewBag.Schools = new SelectList(_unitOfWork.Schools.FindAll(), "Id", "Name", disabilitySchool.SchoolId);
+            ViewBag.Disabilities = new SelectList(_unitOfWork.Disabilitys.FindAll(), "Id", "Name", disabilitySchool.DisabilityId);
+            return View(disabilitySchool);
+        }
+
+        [HttpPost]
+        public IActionResult DeletePostDisabilitySchool(int id)
+        {
+            var disabilitySchool = _unitOfWork.DisabilitySchools.FindById(id);
+            _unitOfWork.DisabilitySchools.Delete(disabilitySchool);
+            _unitOfWork.Save();
+
+            TempData["Delete"] = "تم حذف البيانات بنجاح";
+            return RedirectToAction(nameof(DisabilitySchool));
+        }
+    }
+}
